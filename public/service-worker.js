53d39ae5234905daf5248937a4c8f09fbaa0df62
Michael Li <thisislimichael@gmail.com>
2022-02-07 23:08:56 -0800

    registered service worker

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+const APP_PREFIX = 'BudgerTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+
+const FILES_TO_CACHE = [
+  "./index.html",
+];
+
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
